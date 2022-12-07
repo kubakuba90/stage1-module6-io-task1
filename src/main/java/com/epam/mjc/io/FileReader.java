0de4f1bba3b4d0d0aac0fb6d11599f71c7241eb8
kubakuba90 <fotograflubin@gmail.com>
2022-12-07 17:54:39 +0100

    All task done.

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,53 @@
 package com.epam.mjc.io;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
 
 
 public class FileReader {
 
-    public Profile getDataFromFile(File file) {
-        return new Profile();
+    public static Profile getDataFromFile(File file) throws IOException { //throws IOException brak
+
+        FileInputStream in = null;
+        FileOutputStream out = null;
+        StringBuilder dataFromFile = new StringBuilder();
+        String stringWithProfile = "";
+
+        try {
+            in = new FileInputStream(file);
+
+
+            int c;
+            while ((c = in.read()) != -1) {
+                dataFromFile.append((char) c);
+
+                // read byte from file so its necessary to cast
+                stringWithProfile += ((char) c);
+
+            }
+        } finally {
+            if (in != null) {
+                in.close();
+            }
+            if (out != null) {
+                out.close();
+            }
+        }
+
+        System.out.println(stringWithProfile);
+
+        String[] keyVals = stringWithProfile.split("[:\\n]");
+
+
+            return new Profile(keyVals[1].trim(),
+                    Integer.parseInt(keyVals[3].trim()),
+                    keyVals[5].trim(),
+                    Long.parseLong(keyVals[7].trim()));
+
     }
 }
+
