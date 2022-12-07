1b94b140d173f720fa40aa3078c1307b9558d00e
kubakuba90 <fotograflubin@gmail.com>
2022-12-07 20:38:19 +0100

    All task done. Added proper catch IOException, after quality modifications

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,21 +1,16 @@
 package com.epam.mjc.io;
 
 import java.io.*;
-import java.util.Arrays;
-import java.util.HashMap;
-
-
 public class FileReader {
 
-    public static Profile getDataFromFile(File file)  { //throws IOException brak
+    public static Profile getDataFromFile(File file) { //throws IOException brak
 
-        FileInputStream in = null;
-        FileOutputStream out = null;
         StringBuilder dataFromFile = new StringBuilder();
         String stringWithProfile = "";
 
-        try {
-            in = new FileInputStream(file);
+        try (FileInputStream in = new FileInputStream(file);
+                ) {
+            //in = new FileInputStream(file);
             int c;
             while ((c = in.read()) != -1) {
                 dataFromFile.append((char) c);
@@ -23,28 +18,15 @@ public class FileReader {
                 stringWithProfile += ((char) c);
             }
         } catch (IOException e) {
-            System.out.println();
-        }
-        finally {
-            try {
-                if (in != null) {
-                    in.close();
-                }
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        }
 
-        System.out.println(stringWithProfile);
+        }
 
         String[] keyVals = stringWithProfile.split("[:\\n]");
-
 
             return new Profile(keyVals[1].trim(),
                     Integer.parseInt(keyVals[3].trim()),
                     keyVals[5].trim(),
                     Long.parseLong(keyVals[7].trim()));
-
     }
 }
 
